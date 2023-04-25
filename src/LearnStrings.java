91dbaac2be97f35c3990c3a2085323c12bdb50ec
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-25 11:49:49 -0500

    feat: char at method

--- src/LearnStrings.java
+++ src/LearnStrings.java
@@ -1,31 +1,7 @@
 public class LearnStrings {
-
-    public static String split(String text) {
-        return text.replace(" ", "");
-    }
-
     public static void main(String[] args) {
-        String firstName = "  Edwin  ";
+        String myText = "Apple";
 
-        System.out.format("'%s'", firstName);
-        System.out.println();
-        System.out.format("'%s'", firstName.strip());
-        System.out.println();
-        System.out.format("'%s'", firstName.stripLeading());
-        System.out.println();
-        System.out.format("'%s'", firstName.stripTrailing());
-        System.out.println();
-        System.out.format("'%s'", firstName.trim());
-
-        System.out.println();
-
-        String myLongText = """
-                        first line
-                                    second line
-                third line
-                """;
-
-        System.out.println(myLongText);
-        System.out.format("'%s'",myLongText.stripIndent());
+        System.out.println(myText.charAt(3));
     }
 }
