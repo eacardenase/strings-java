016b14cade70d8b797555f84d7cfb933d19bfe40
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-25 10:58:51 -0500

    feat: string builder and string buffer

--- src/LearnStrings.java
+++ src/LearnStrings.java
@@ -5,7 +5,26 @@ public class LearnStrings {
         String vegetable = new String("broccoli");
         String anotherVegetable = new String("broccoli"); // don't point to vegetable reference
 
-        System.out.println(fruit == anotherFruit);
-        System.out.println(vegetable == anotherVegetable);
+//        System.out.println(fruit == anotherFruit);
+//        System.out.println(vegetable == anotherVegetable);
+
+        String text1 = "this is my text1";
+        String text2 = "this is my text2";
+
+        String finalString = new StringBuilder(text1.length() + text2.length() + 1)
+                .append(text1)
+                .append(" ")
+                .append(text2)
+                .toString();
+
+        System.out.println(finalString);
+
+        String otherFinalString = new StringBuffer()
+                .append(text1)
+                .append(" ")
+                .append(text2)
+                .toString();
+
+        System.out.println(otherFinalString);
     }
 }
