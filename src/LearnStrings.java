da90dd2b6ac237d7c2608e370133a08fe95f4f52
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-25 12:02:10 -0500

    feat: compare to and compare to ignore case methods

--- src/LearnStrings.java
+++ src/LearnStrings.java
@@ -1,7 +1,20 @@
 public class LearnStrings {
     public static void main(String[] args) {
-        String myText = "Apple";
+        String firstWord = "Apple";
+        String secondWord = "Banana";
 
-        System.out.println(myText.charAt(3));
+        // compares alphabetically
+        System.out.println(firstWord.compareTo(secondWord)); // -1
+        System.out.println(secondWord.compareTo(firstWord)); // 1
+
+        System.out.println(firstWord.compareTo("apple")); // -32
+        System.out.println(firstWord.compareToIgnoreCase("apple")); // 0
+    }
+
+    public static Integer compareToIgnoreCaseStrip(String text1, String text2) {
+        String lowerCaseText1 = text1.strip().toLowerCase();
+        String lowerCaseText2 = text2.strip().toLowerCase();
+
+        return lowerCaseText1.compareTo(lowerCaseText2);
     }
 }
