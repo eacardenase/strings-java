1fc67c4d11af049d18fe0ededbd13772a353b5b7
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-25 17:06:50 -0500

    feat: equals vs content equals methods

--- src/LearnStrings.java
+++ src/LearnStrings.java
@@ -1,8 +1,13 @@
 public class LearnStrings {
     public static void main(String[] args) {
-        String fileName = "  file001.txt   ".strip();
+        String firstText = "apple";
+        String secondText = "apple";
+        StringBuffer thirdText = new StringBuffer("apple");
+        StringBuilder fourthText = new StringBuilder("apple");
 
-        System.out.println(fileName.endsWith("txt"));
-        System.out.println(fileName.startsWith("file"));
+        System.out.println(firstText.contentEquals(secondText)); // compares not only strings
+        System.out.println(firstText.contentEquals(thirdText)); // it also compares char sequences
+        System.out.println(firstText.contentEquals(fourthText));
+        System.out.println(firstText.equals(secondText)); // compares two strings
     }
 }
