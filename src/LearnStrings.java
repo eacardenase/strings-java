ee14bb173e05ef04242101d57445c22b0c400290
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-25 12:04:25 -0500

    feat: contains method

--- src/LearnStrings.java
+++ src/LearnStrings.java
@@ -1,20 +1,9 @@
 public class LearnStrings {
     public static void main(String[] args) {
-        String firstWord = "Apple";
-        String secondWord = "Banana";
+        String myText = "Four score and seven years ago";
 
-        // compares alphabetically
-        System.out.println(firstWord.compareTo(secondWord)); // -1
-        System.out.println(secondWord.compareTo(firstWord)); // 1
+        System.out.println(myText);
+        System.out.println(myText.contains("ago"));
 
-        System.out.println(firstWord.compareTo("apple")); // -32
-        System.out.println(firstWord.compareToIgnoreCase("apple")); // 0
-    }
-
-    public static Integer compareToIgnoreCaseStrip(String text1, String text2) {
-        String lowerCaseText1 = text1.strip().toLowerCase();
-        String lowerCaseText2 = text2.strip().toLowerCase();
-
-        return lowerCaseText1.compareTo(lowerCaseText2);
     }
 }
