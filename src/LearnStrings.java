ca9574478f3a44c436de6895ac824766b1cf0ab1
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-25 16:56:05 -0500

    feat: starts and ends with method

--- src/LearnStrings.java
+++ src/LearnStrings.java
@@ -1,23 +1,8 @@
 public class LearnStrings {
     public static void main(String[] args) {
-        String text = """
-                Smith,Fred,1/1/79,1111 ABC St.,Apple,CA
-                McGuire,Jerry,2/2/80,2222 DEF St.,Orange,NV
-                Flintstone,Fred,3/3/81,3333 GHI St.,Pear,MO
-                """;
+        String fileName = "  file001.txt   ".strip();
 
-        String[] people = text.split("\n");
-
-        System.out.println(people.length); // property
-        System.out.println("people".length()); // method
-
-        String[] person = people[0].split(",");
-
-        System.out.println(people[0]);
-
-        System.out.println(person[0]);
-        System.out.println(person[1]);
-        System.out.println(person[2]);
-        System.out.println(person[3]);
+        System.out.println(fileName.endsWith("txt"));
+        System.out.println(fileName.startsWith("file"));
     }
 }
