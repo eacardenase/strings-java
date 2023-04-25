f2361aa63534871a632ebfa8b7afe10c9d56374b
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-25 11:06:10 -0500

    feat: lower and upper case

--- src/LearnStrings.java
+++ src/LearnStrings.java
@@ -1,30 +1,8 @@
 public class LearnStrings {
     public static void main(String[] args) {
-        String fruit = "banana";  // string literal
-        String anotherFruit = "banana"; // points to the same reference as 'fruit'
-        String vegetable = new String("broccoli");
-        String anotherVegetable = new String("broccoli"); // don't point to vegetable reference
+        String myText = "AbCdEfG";
 
-//        System.out.println(fruit == anotherFruit);
-//        System.out.println(vegetable == anotherVegetable);
-
-        String text1 = "this is my text1";
-        String text2 = "this is my text2";
-
-        String finalString = new StringBuilder(text1.length() + text2.length() + 1)
-                .append(text1)
-                .append(" ")
-                .append(text2)
-                .toString();
-
-        System.out.println(finalString);
-
-        String otherFinalString = new StringBuffer()
-                .append(text1)
-                .append(" ")
-                .append(text2)
-                .toString();
-
-        System.out.println(otherFinalString);
+        System.out.println(myText.toUpperCase());
+        System.out.println(myText.toLowerCase());
     }
 }
